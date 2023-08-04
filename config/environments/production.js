a5cd0b26a9439a68a1364e5c96847fe101f4cf6f
hacheDJ <this.hacheDJ.dejotahachece@gmail.com>
2023-08-04 00:09:19 -0500

    se agrega comando start

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 module.exports = {
     PORT: process.env.PORT_P,
     DB: {
-        host: process.env.HOST_P,
-        database: process.env.DATABASE_P,
-        username: process.env.USER_NAME_P,
-        password: process.env.PASSWORD_P
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        username: process.env.USER_NAME,
+        password: process.env.PASSWORD
     }
 }
